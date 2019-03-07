3115e29e8fa1191e692cd128b8529374734b90d8
Sean Kung <seankung@Seans-MacBook-Pro.local>
2019-03-07 09:38:58 -0600

    Removes remote calls for simpler redirect

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -5,6 +5,7 @@ class OrderItemsController < ApplicationController
     if @order.save
       flash[:notice] = "Item added to cart"
       session[:order_id] = @order.id
+      redirect_to "/"
     else
       flash[:alert] = "The Item could not be added to cart"
     end
@@ -15,6 +16,8 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
     @order_items = @order.order_items
+
+    redirect_to '/cart'
   end
 
   def destroy
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
 
   validates_presence_of :name, :description, :price
 
-  validates_length_of :title, maximum: 250
+  validates_length_of :name, maximum: 250
   validates_length_of :description, maximum: 1000
 
   validates_numericality_of :price

f53ae05db367639b013f2ffde953e0d7a4ec97b7
Sean Kung <seankung@Seans-MacBook-Pro.local>
2019-03-10 13:29:11 -0500

    Adds migration and updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   get 'order_items/destroy'
 
   get 'carts/show'
+  resources :orders
 
   resources :items
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
+  resources :users, only: [:show]
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
